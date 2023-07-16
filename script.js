d4325dc2ba8a7fd3a5c0eef66a4b3a2f6eb08e41
Taylor Treadwell <taylortread91@gmail.com>
2023-07-16 12:55:43 -0500

    Created rudementary fire method in human ship class instance

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ Your spaceship, the USS Assembly should have the following properties:
 hull - 20
 firepower - 5
 accuracy - .7
+
+ * REMEMBER BIND YOUR METHODS IF YOU PLAN TO USE THEM AS EVENT Listeners !!!!!!!
+ 
 */
 class Ship{
     constructor(hull,firepower,accuracy){
@@ -28,30 +31,38 @@ class Ship{
 class HumanShip extends Ship{
     constructor(hull,firepower,accuracy){
         super(hull,firepower,accuracy);
-        hull = 20;
-        firepower = 5;
-        accuracy = .7;
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = .7;
+    }
+    fire(human,alien){
+        console.log('alien hull ' + alien.hull);
+        console.log('human hull ' + human.hull)
+        if(Math.random() <= this.accuracy){
+        alien.hull -= human.firepower;
+        }
+        else{
+            console.log('I fired, and I missed')
+        }
+        console.log('alien hull ' + alien.hull);
     }
 }
-/**
- * REMEMBER BIND YOUR METHODS IF YOU PLAN TO USE THEM AS EVENT Listeners !!!!!!!
- */
-const S1 = new HumanShip(1,5,9);
 
-
+const S1 = new HumanShip();
 /*
 hull - between 3and 6
 firepower - between 2and 4
 accuracy - between .6and .8
 */
 class AlienShip extends Ship{
-    constructor(hull,firepower,accuracy)
+    constructor(hull,savedHull,firepower,accuracy)
     {
         super(hull,firepower,accuracy);
-        hull = Math.floor(Math.random() * (7-3) + 3);
-        firepower = Math.floor(Math.random() * (5-2) + 2);
-        accuracy = Math.random() * (.9 - .6) + .6;
+        this.hull = Math.floor(Math.random() * (7-3) + 3);
+        this.firepower = Math.floor(Math.random() * (5-2) + 2);
+        this.accuracy = Math.random() * (.9 - .6) + .6;
     }
 }
 const AS1 = new AlienShip();
 
+S1.fire(S1,AS1);
