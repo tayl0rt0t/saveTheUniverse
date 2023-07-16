fbfc6fab3dbb6f704659e9a4a72404057b9b8854
Taylor Treadwell <taylortread91@gmail.com>
2023-07-16 16:21:29 -0500

    created alien fire method, created 6 aliens pushed into ALIENS array

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ accuracy - .7
  * REMEMBER BIND YOUR METHODS IF YOU PLAN TO USE THEM AS EVENT Listeners !!!!!!!
  
 */
+const ps = require("prompt-sync")
+const prompt = ps();
 class Ship{
     constructor(hull,firepower,accuracy){
         this.hull = hull;
@@ -37,9 +39,11 @@ class HumanShip extends Ship{
     }
     fire(human,alien){
         console.log('alien hull ' + alien.hull);
-        console.log('human hull ' + human.hull)
+        console.log('human hull ' + human.hull);
         if(Math.random() <= this.accuracy){
         alien.hull -= human.firepower;
+        console.log('YOU HIT THE ALIEN!')
+        console.log('Alien hull damage report: Alien hull is now at ' + alien.hull + '%');
         }
         else{
             console.log('I fired, and I missed')
@@ -55,14 +59,31 @@ firepower - between 2and 4
 accuracy - between .6and .8
 */
 class AlienShip extends Ship{
-    constructor(hull,savedHull,firepower,accuracy)
+    constructor(hull,firepower,accuracy)
     {
         super(hull,firepower,accuracy);
         this.hull = Math.floor(Math.random() * (7-3) + 3);
         this.firepower = Math.floor(Math.random() * (5-2) + 2);
         this.accuracy = Math.random() * (.9 - .6) + .6;
     }
+    fire(alien,human){
+        // console.log(this.accuracy);
+        // console.log(human.accuracy);
+        if(Math.random() <= alien.accuracy){
+            human.hull -= alien.firepower;
+            console.log("YOU'VE BEEN HIT!")
+            console.log("DAMAGE REPORT! SHIELDS AT " + human.hull + "%");
+        }
+    }
 }
 const AS1 = new AlienShip();
+const AS2 = new AlienShip();
+const AS3 = new AlienShip();
+const AS4 = new AlienShip();
+const AS5 = new AlienShip();
+const AS6 = new AlienShip();
+const ALIENS = [];
+ALIENS.push(AS1,AS2,AS3,AS4,AS5,AS6)
 
-S1.fire(S1,AS1);
+
+
